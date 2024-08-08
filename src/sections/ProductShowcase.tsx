0be70347221f004904edca35d27b476beefe5880
Chaturved Degloorkar <chaturved.edu@gmail.com>
2024-08-08 20:51:31 +0530

    Pricing section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -7,12 +7,12 @@ export const ProductShowcase = () => {
   return (
     <section className="bg-gradient-to-b from-[#ffffff] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <div className="max-w-[540px] mx-auto">
+        <div className="section-heading">
           <div className="flex justify-center">
             <div className="tag">Boost your productivity</div>
           </div>
-          <h1 className="mt-5 text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text">A more effective way to track progress</h1>
-          <p className="mt-5 text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E]">Effortlessly turn your ideas into a fully functional, responsive, no-code SaaS website in just minutes with the set of free components for Framer.</p>
+          <h1 className="mt-5 section-title">A more effective way to track progress</h1>
+          <p className="mt-5 section-description">Effortlessly turn your ideas into a fully functional, responsive, no-code SaaS website in just minutes with the set of free components for Framer.</p>
         </div>
         <div className="relative">
           <Image src={productImage} alt='Product image' className="mt-10" />
